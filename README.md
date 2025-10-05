618d5b9d9e1badc162d2925a2a34a390f7bd31b8
Ainoa <aimmolina2@gmail.com>
2025-10-05 20:09:42 +0200

    generarNumeroAleatorio and obtenerPuntosCarta testing ready

--- README.md
+++ README.md
@@ -19,3 +19,9 @@ Pero, también podemos testear que mensaje se nos muestra cuando no hemos alcanz
 Creamos los archivos _ui.spec.ts_ y _motor.spec.ts_ para poder desarrollar los tests.
 
 ## 3. Pruebas unitarias para comprobar las funciones que generan número aleatorio y asignacion a la carta
+
+En `motor.spec.ts` hacemos los test que nos ayudarán a:
+
+- Probar que el número aleatorio que se genera funciona correctamente testeando la función `generarNumeroAleatorio`. Comprobamos que cuando el número random es un 0 nos devuelve un 1, y si lo que queremos es generar un 10 el número random debe ser un 0.9999 (el número random que se genera es entre 0 y 1).
+- Probar que cuando este número aleatorio es mayor que 7 sume 2. Para ello hacemos test de cuando el número aleatorio es 9 suma 2, cuando es 7 no suma nada y cuando es 4 tampoco suma.
+- Probar que cuando el jugador obtiene una carta se genere el valor de esa carta de forma correcta, que lo encontramos en la funcion `obtenerPuntosCarta`. Testeamos que si la carta es 3 la puntuación es 3, si la carta es 7 la puntuación es 7 y si la carta es 11 la puntuación es 0.5.
--- src/motor.spec.ts
+++ src/motor.spec.ts
@@ -1,6 +1,68 @@
-import { obtenerMensajeCuandoMePlanto } from "./motor";
+import {
+  obtenerMensajeCuandoMePlanto,
+  generarNumeroAleatorio,
+  generarNumeroCarta,
+  obtenerPuntosCarta,
+} from "./motor";
 import { partida } from "./model";
 import { vi } from "vitest";
+
+describe("obtenerPuntosCarta", () => {
+  it("Cuando la carta es un 3, debería devolver 3", () => {
+    const carta: number = 3;
+    const puntosEsperados: number = 3;
+    const resultado: number = obtenerPuntosCarta(carta);
+    expect(resultado).toBe(puntosEsperados);
+  });
+  it("Cuando la carta es un 7, debería devolver 7", () => {
+    const carta: number = 7;
+    const puntosEsperados: number = 7;
+    const resultado: number = obtenerPuntosCarta(carta);
+    expect(resultado).toBe(puntosEsperados);
+  });
+  it("Cuando la carta es un 11, debería devolver 0.5", () => {
+    const carta: number = 11;
+    const puntosEsperados: number = 0.5;
+    const resultado: number = obtenerPuntosCarta(carta);
+    expect(resultado).toBe(puntosEsperados);
+  });
+});
+
+describe("generarNumeroAleatorio", () => {
+  it("MathRandom lo forzamos para que devuelva 1, debería devolver 1", () => {
+    const numeroEsperado: number = 1;
+    vi.spyOn(global.Math, "random").mockReturnValue(0);
+    const resultado: number = generarNumeroAleatorio();
+    expect(resultado).toBe(numeroEsperado);
+  });
+  it("MathRandom lo forzamos para que devuelva 10, debería devolver 10", () => {
+    const numeroEsperado: number = 10;
+    vi.spyOn(global.Math, "random").mockReturnValue(0.9999999);
+    const resultado: number = generarNumeroAleatorio();
+    expect(resultado).toBe(numeroEsperado);
+  });
+});
+
+describe("generarNumeroCarta", () => {
+  it("Cuando el número aleatorio es 9, debería devolver la carta con puntuación 11", () => {
+    const numeroAleatorio: number = 9;
+    const numeroCartaEsperado: number = 11;
+    const resultado: number = generarNumeroCarta(numeroAleatorio);
+    expect(resultado).toBe(numeroCartaEsperado);
+  });
+  it("Cuando el número aleatorio es 7, debería devolver la carta con puntuación 7", () => {
+    const numeroAleatorio: number = 7;
+    const numeroCartaEsperado: number = 7;
+    const resultado: number = generarNumeroCarta(numeroAleatorio);
+    expect(resultado).toBe(numeroCartaEsperado);
+  });
+  it("Cuando el número aleatorio es 4, debería devolver la carta con puntuación 4", () => {
+    const numeroAleatorio: number = 4;
+    const numeroCartaEsperado: number = 4;
+    const resultado: number = generarNumeroCarta(numeroAleatorio);
+    expect(resultado).toBe(numeroCartaEsperado);
+  });
+});
 
 describe("obtenerMensajeCuandoMePlanto", () => {
   it("Debería devolver 'Has sido muy conservador' si la puntuación es 3", () => {
